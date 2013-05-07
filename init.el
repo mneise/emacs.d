0ed2a343678da19de2b047d92b4f15926338fcb0
Maria Neise <maria.neise1@gmail.com>
2013-05-08 08:07:29 +1200

    Load path.

--- init.el
+++ init.el
@@ -1,5 +1,11 @@
-;; ========== Packaging ==========
+(defun c/set-exec-path-from-shell-PATH ()
+  (let ((path-from-shell 
+         (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+(c/set-exec-path-from-shell-PATH)
 
+;; ========== Packaging ==========
 ;; add package repositories
 (require 'package)
 (package-initialize)
